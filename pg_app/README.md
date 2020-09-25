b38421d057dfc3af7ae1107e174298978ba339e5
Julia Byström <juliabys@kth.se>
2020-09-25 21:07:07 +0200

    initial commit

---
+++ pg_app/README.md
@@ -0,0 +1,10 @@
+# pg_app
+
+A new Flutter project.
+
+## Dependencies:
+cloud_firestore
+Read more: https://pub.dev/packages/cloud_firestore/install
+
+firebase_auth
+Read more: https://pub.dev/packages/firebase_auth
---
+++ pg_app/lib/screens/root.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:pg_app/screens/Login/login.dart';
+
+class Root extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Login(),
+    );
+  }
+}
